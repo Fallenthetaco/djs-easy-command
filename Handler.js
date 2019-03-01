ec9d092d406af226daab69c2090db7fcd3df0316
Fallenthetaco <nghjame62@gmail.com>
2019-02-28 22:49:16 -0800

    Update Handler.js

--- Handler.js
+++ Handler.js
@@ -84,11 +84,14 @@ class Handler {
                 return message.reply(`Oops, this shouldn't happen, please contact ${this.Client.owners.length < 1 ?
                 'the bot owners' : this.Client.owners.map(o => !message.client.users.get(o) ? o :
                     message.client.users.get(o).tag).join(', or ')}. Here's the error\n\n\`${err.message}\``)
-
-                client.users.get('286713468285878272').send(`Command: ${command.name}, ${err.message}`)
+                try {
+                    client.users.get('286713468285878272').send(`Command: ${command.name}, ${err.message}`)
+                } catch (err) {
+                    console.error(err);
+                }
+                timestamps.set(message.author.id, now);
+                setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
             }
-            timestamps.set(message.author.id, now);
-            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
         }
     }
     getCommand(command) {
